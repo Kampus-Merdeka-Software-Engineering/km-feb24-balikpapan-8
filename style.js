0baaae6b7f01e9c1fdb8294ebfe34fab8c956629
unknown <helyxrulk@gmail.com>
2024-05-27 17:39:43 +0700

    cleancode index.js

--- style.js
+++ style.js
@@ -1,4 +1,3 @@
-// Toggle button functionality
 document.getElementById("toggle-button").addEventListener("click", () => {
   const navbarLinks = document.getElementById("navbar-links");
   const toggleButton = document.getElementById("toggle-button");
@@ -11,24 +10,22 @@ function darkmodeToggle() {
   let salesperformance = document.getElementsByClassName("range");
   let body = document.body;
 
-  // Set font color to black for salesperformance elements initially
   for (let i = 0; i < salesperformance.length; i++) {
     salesperformance[i].style.color = "black";
     salesperformance[i].style.transition = "color 0.5s";
   }
+
   body.style.transition = "background-color 0.5s";
   darkmodeCheckbox.addEventListener("change", () => {
     if (darkmodeCheckbox.checked) {
       console.log("Dark mode is on");
-      body.style.background = "linear-gradient(180deg, #000000, #ffff00)"; // Black to yellow gradient when dark mode is on
-      // Set font color to white for salesperformance elements when dark mode is on
+      body.style.background = "linear-gradient(180deg, #000000, #ffff00)";
       for (let i = 0; i < salesperformance.length; i++) {
         salesperformance[i].style.color = "white";
       }
     } else {
       console.log("Dark mode is off");
-      body.style.background = "linear-gradient(180deg, #ffffff, #ffff00)"; // White to yellow gradient when dark mode is off
-      // Set font color to black for salesperformance elements when dark mode is off
+      body.style.background = "linear-gradient(180deg, #01cbfd, #fbff00)";
       for (let i = 0; i < salesperformance.length; i++) {
         salesperformance[i].style.color = "black";
       }
